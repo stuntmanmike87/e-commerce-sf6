c845d456376497026632958bd1620e525086c4b9
stuntmanmike87 <mich.perigaud@gmail.com>
2023-04-25 17:39:00 +0200

    whole changes and updates, with PHPStan and Rector ok

---
+++ tests/object-manager.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Kernel;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+//Usage of super global $_SERVER found; Usage of GLOBALS are discouraged
+//consider not relying on global scope
+
+$kernel->boot();
+
+/** @var ManagerRegistry $doctrine */
+$doctrine = $kernel->getContainer()->get('doctrine');
+
+return $doctrine->getManager();
